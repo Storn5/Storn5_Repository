e743f5a9698b2a38ebe28c2ecc9998f12b1fe5c7
Storn5 <storn5@ukr.net>
2018-09-18 23:53:10 +0300

    Reorganised Labs folder

---
+++ Labs/CS50/2/initials.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    string name = get_string();
+    if(name[0] >= 'a' && name[0] <= 'z') {
+        printf("%c", name[0] - ('a' - 'A'));
+    }
+    else {
+        printf("%c", name[0]);
+    }
+    for(int i = 0; i < strlen(name); i ++) {
+        if(name[i] == ' ') {
+            if(name[i+1] >= 'a' && name[i+1] <= 'z') {
+                printf("%c", name[i+1] - ('a' - 'A'));
+            }
+            else {
+                printf("%c", name[i+1]);
+            }
+        }
+    }
+    printf("\n");
+}
